28683fd644b4e77a784488b740038f6a942875ea
Cc <3300833815@qq.com>
2026-04-06 23:38:07 +0800

    重写摄像头IIC通信

---
+++ include/protocol/IIC/IIC_camera.h
@@ -0,0 +1,47 @@
+#ifndef CAMERA_IIC_H
+#define CAMERA_IIC_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <stdint.h>
+
+namespace esp32s3
+{
+struct __attribute__((packed)) CameraPackage
+{
+  uint8_t userID;
+  char ssid[32];
+  char password[32];
+  CameraPackage(uint8_t userID = 0, const char* ssid = "",
+                const char* password = "")
+      : userID(userID)
+  {
+    strncpy(this->ssid, ssid, sizeof(this->ssid) - 1);
+    this->ssid[sizeof(this->ssid) - 1] = '\0';
+    strncpy(this->password, password, sizeof(this->password) - 1);
+    this->password[sizeof(this->password) - 1] = '\0';
+  }
+};
+struct __attribute__((packed)) SlaveStatus
+{
+  uint8_t isReceived;   // 0x01
+  uint8_t isSetWifi;    // 0x02
+  uint8_t isgetUserID;  // 0x04
+};
+class Camera_IIC
+{
+  CameraPackage cameraPacket_;
+  bool isAllReady_;
+  int address_;
+  SlaveStatus slaveStatus_;
+
+ public:
+  Camera_IIC(int sdaPin, int sclPin, uint32_t frequency, int address);
+  byte sendPacket(const CameraPackage& cameraPacket);
+  uint8_t requestStatus();
+
+ private:
+  void searchError(byte error);
+};
+}  // namespace esp32s3
+#endif  // CAMERA_IIC_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,40 @@
 
 #include "LogSwitch.h"
 #include "app/AppModule.h"
-
+#include "protocol/IIC/IIC_camera.h"
+#define CAMERA1_IIC_ADDRESS 0x42
+#define CAMERA2_IIC_ADDRESS 0x43
+#define IIC_SCL_PIN 9
+#define IIC_SDA_PIN 8
+#define IIC_FREQUENCY 100000
 void setup()
 {
   Serial.begin(115200);
   delay(1000);
-  if (!esp32s3::AppModule::boot())
+  // if (!esp32s3::AppModule::boot())
+  // {
+  //   LOG_PRINTLN(LOG_WIFI, "[Main] module init/start has failures");
+  // }
+  // Keep the IIC master object alive for the full application lifetime.
+  static esp32s3::Camera_IIC iic(IIC_SDA_PIN, IIC_SCL_PIN, IIC_FREQUENCY,
+                                 CAMERA1_IIC_ADDRESS);
+  esp32s3::CameraPackage cameraPacket(1, "Redmi", "88889999");
+  byte error;
+  do
   {
-    LOG_PRINTLN(LOG_WIFI, "[Main] module init/start has failures");
-  }
+    error = iic.sendPacket(cameraPacket);
+    delay(500);
+  } while (error != 0);
+  uint8_t status;
+  do
+  {
+    status = iic.requestStatus();
+    delay(500);
+  } while (status != 0x00);
 }
 
 void loop()
 {
   // 避免空转占满 CPU，保证系统任务调度稳定
-  vTaskDelay(pdMS_TO_TICKS(1000));
+  delay(100);
 }
